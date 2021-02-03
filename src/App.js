5a000dec74f84b17da77c8452c4d79ea960d2c03
miracoly <68049792+miracoly@users.noreply.github.com>
2021-02-03 23:09:33 +0100

    update menu-item to navigate with withRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
 
 const HatsPage = () => (
   <div>
     <h1>Hats Page</h1>
   </div>
-)
+);
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
