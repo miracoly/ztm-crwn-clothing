5ffd2abbedd9865f031ba506dce069e91bf1aa0c
miracoly <68049792+miracoly@users.noreply.github.com>
2021-02-03 20:57:10 +0100

    Adding Prettier and first route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
