96d19c446ebf522798efef2e0150bf0051c335cb
miracoly <68049792+miracoly@users.noreply.github.com>
2021-02-14 13:18:38 +0100

    adding Sign-Up with E-Mail and Password

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import SignInAndUp from "./pages/sign-in-and-up/sign-in-and-up";
-import { auth } from "./firebase/firebase";
+import { auth, createUserProfileDocument } from "./firebase/firebase";
 
 class App extends React.Component {
   constructor() {
@@ -17,10 +17,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-        console.log(user);
-      });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+          console.log(this.state);
+        })
+      } else {
+        this.setState({currentUser: userAuth});
+      }
+    });
   }
 
   componentWillUnmount() {
--- src/pages/sign-in-and-up/sign-in-and-up.jsx
+++ src/pages/sign-in-and-up/sign-in-and-up.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 import "./sign-in-and-up.scss";
 
 const SignInAndUp = () => (
   <div className="sign-in-and-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
