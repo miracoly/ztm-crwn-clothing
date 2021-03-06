f0c73eae81b3915bd95d1e0b918e40c4c3baa2c4
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-06 19:36:47 +0100

    refactoring directory

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,26 +1,10 @@
 import React from "react";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-import SHOP_DATA from "./shop-data";
+import CollectionOverview from "../../components/collection-overview/collection-overview";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionOverview />
+  </div>
+);
 
 export default ShopPage;
